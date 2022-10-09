6f256da98c3f86de7ae7710e786ce594e63fd29a
Yan Gonçalves <fgyan94@gmail.com>
2022-10-09 18:59:03 -0300

    feat: add company service find one

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common'
 import { Injectable } from '@nestjs/common/decorators'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
@@ -6,6 +7,14 @@ import { Company } from './entities/company.entity'
 @Injectable()
 export class CompaniesService {
   constructor(@InjectRepository(Company) private readonly repo: Repository<Company>) {}
+
+  async get(id: number): Promise<Company> {
+    try {
+      return await this.repo.findOneOrFail({ where: { id } })
+    } catch {
+      throw new NotFoundException(`Company with id: '${id}' not found`)
+    }
+  }
 
   async find(): Promise<Company[]> {
     return await this.repo.find()
