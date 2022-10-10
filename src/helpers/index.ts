4c2b4d2af6d8c5f576eef28a980b5728bdc551b9
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:31:27 -0300

    feat: add sanitize helper functions

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,21 @@
 import { AddressDto } from '../common/dtos/address.dto'
 
+export const sanitizeCnpj = (cnpj: string) => {
+  return cnpj.replace(/\D/g, '').replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g, '$1.$2.$3/$4-$5')
+}
+
+export const sanitizeCPF = (cpf: string) => {
+  return cpf.replace(/\D/g, '').replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4')
+}
+
+export const sanitizeCep = (cep: string) => {
+  return cep.replace(/\D/g, '')
+}
+
 export const fetchAddress = async ({ cep, num }: AddressDto) => {
   const url = process.env.CEP_API ?? 'https://viacep.com.br/ws'
-  const cepApi = `${url}/${cep}/json`
+  const sanitizedCep = sanitizeCep(cep)
+  const cepApi = `${url}/${sanitizedCep}/json`
   return await fetch(cepApi)
     .then((data) => data.json())
     .then(({ logradouro, complemento, bairro, localidade, uf, cep }) => {
