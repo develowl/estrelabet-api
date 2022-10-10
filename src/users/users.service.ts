65d7e272b0754a8562ec4cb78c5544dc6b2f2e5a
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:12:32 -0300

    feat: add get user function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
+import { NotFoundException } from '@nestjs/common'
 import { Injectable } from '@nestjs/common/decorators'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+
+  async get(id: number): Promise<User> {
+    try {
+      return await this.repo.findOneByOrFail({ id })
+    } catch {
+      throw new NotFoundException('User not found')
+    }
+  }
+}
