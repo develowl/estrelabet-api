29c5f3d795c17c8fdfa636aa5a1477c4d477b43a
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:13:10 -0300

    test: add get user function

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import mockFetch from 'jest-fetch-mock'
+import { Repository } from 'typeorm'
+import { mockUser } from '../utils/mock/users'
+import { User } from './entities/user.entity'
+import { UsersService } from './users.service'
+
+describe('UsersService', () => {
+  let usersService: UsersService
+  let mockRepository: Repository<User>
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository
+        }
+      ]
+    }).compile()
+
+    usersService = module.get<UsersService>(UsersService)
+    mockRepository = module.get<Repository<User>>(getRepositoryToken(User))
+  })
+
+  afterEach(() => {
+    mockFetch.mockClear()
+    jest.clearAllMocks()
+  })
+
+  it('should be defined', () => {
+    expect(usersService).toBeDefined()
+  })
+
+  describe('get', () => {
+    it('should return a valid company', async () => {
+      const spyGet = jest.spyOn(usersService, 'get')
+      jest.spyOn(mockRepository, 'findOneByOrFail').mockResolvedValueOnce(mockUser)
+
+      expect(await usersService.get(mockUser.id)).toBe(mockUser)
+      expect(mockRepository.findOneByOrFail).toHaveBeenCalledTimes(1)
+      expect(spyGet).toHaveBeenCalledWith(mockUser.id)
+    })
+  })
+})
