e41fe0a0e3716e1a91f7075c62c1f017d34e672f
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 08:55:10 -0300

    feat: add delete company function

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, Param, Post, Put } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from '@nestjs/common'
 import { CompaniesService } from './companies.service'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { UpdateCompanyDto } from './dto/update-company.dto'
@@ -29,5 +29,10 @@ export class CompaniesController {
     @Body() updateCompanyDto: UpdateCompanyDto
   ): Promise<Company> {
     return await this.service.update(id, updateCompanyDto)
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<Company> {
+    return this.service.delete(id)
   }
 }
