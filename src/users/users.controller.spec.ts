890d4c812b871b41cccc0d3c2ab9c0ba9d810922
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 10:43:31 -0300

    test: add UsersController create function

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { CompaniesService } from '../companies/companies.service'
-import { mockUser } from '../utils/mock/users'
+import { mockCreateUserDto, mockUser } from '../utils/mock/users'
 import { User } from './entities/user.entity'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
@@ -54,6 +54,16 @@ describe('UsersController', () => {
     it('should return an array of valid companies', async () => {
       jest.spyOn(usersService, 'find').mockResolvedValueOnce([mockUser])
       expect(await usersController.find()).toStrictEqual([mockUser])
+    })
+  })
+
+  describe('create', () => {
+    it('should create and return a valid company', async () => {
+      const spyCreate = jest.spyOn(usersController, 'create')
+      jest.spyOn(usersService, 'create').mockResolvedValueOnce(mockUser)
+
+      expect(await usersController.create(mockCreateUserDto)).toStrictEqual(mockUser)
+      expect(spyCreate).toHaveBeenCalledWith(mockCreateUserDto)
     })
   })
 })
