7e9646b334358b285e728b647fd767813f051dde
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 08:19:34 -0300

    test: add get company function

--- src/companies/companies.controller.spec.ts
+++ src/companies/companies.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { mockCompany } from '../utils/mock/company'
 import { CompaniesController } from './companies.controller'
 import { CompaniesService } from './companies.service'
 import { Company } from './entities/company.entity'
 
 describe('CompaniesController', () => {
   let companiesController: CompaniesController
+  let companiesService: CompaniesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,7 +22,8 @@ describe('CompaniesController', () => {
       ]
     }).compile()
 
-    companiesController = await module.get<CompaniesController>(CompaniesController)
+    companiesController = module.get<CompaniesController>(CompaniesController)
+    companiesService = module.get<CompaniesService>(CompaniesService)
   })
 
   afterEach(() => {
@@ -29,5 +32,14 @@ describe('CompaniesController', () => {
 
   it('should be defined', () => {
     expect(companiesController).toBeDefined()
+  })
+
+  describe('get', () => {
+    it('should return a valid company', async () => {
+      const spyGet = jest.spyOn(companiesService, 'get').mockResolvedValueOnce(mockCompany)
+
+      expect(await companiesController.get(mockCompany.id)).toStrictEqual(mockCompany)
+      expect(spyGet).toHaveBeenCalledWith(mockCompany.id)
+    })
   })
 })
