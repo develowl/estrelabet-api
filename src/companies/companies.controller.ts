e36f88997ad996ae23c610cec87345ed1cd96462
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 08:32:35 -0300

    feat: add company create function

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common'
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common'
 import { CompaniesService } from './companies.service'
+import { CreateCompanyDto } from './dto/create-company.dto'
 import { Company } from './entities/company.entity'
 
 @Controller('companies')
@@ -7,12 +8,17 @@ export class CompaniesController {
   constructor(@Inject(CompaniesService) private readonly service: CompaniesService) {}
 
   @Get(':id')
-  async get(@Param(':id') id: number): Promise<Company> {
+  async get(@Param('id') id: number): Promise<Company> {
     return await this.service.get(id)
   }
 
   @Get()
   async find(): Promise<Company[]> {
     return await this.service.find()
+  }
+
+  @Post()
+  async create(@Body() createCompanyDto: CreateCompanyDto): Promise<Company> {
+    return await this.service.create(createCompanyDto)
   }
 }
