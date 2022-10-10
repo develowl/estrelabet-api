0698ed391a4cfae92a45f12fe2d38d00737f9639
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 12:57:06 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule } from '@nestjs/swagger'
+import { DocumentBuilder } from '@nestjs/swagger/dist'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  const configService = app.get(ConfigService)
+
+  const config = new DocumentBuilder()
+    .setTitle('EstrelaBet API')
+    .setDescription('The challenge EstrelaBet API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  const port = Number(configService.get<string>('PORT')) || 3000
+  await app.listen(port)
 }
 
 bootstrap()
