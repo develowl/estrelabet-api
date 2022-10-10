796536da1adbb63bc7d2c564950dc78d2458f007
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 14:52:12 -0300

    feat: add swagger api property in CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsCPF } from 'brazilian-class-validator'
 import { IsEmail, IsNotEmpty, IsPhoneNumber, IsPositive, IsString } from 'class-validator'
 import { AddressDto } from '../../common/dtos/address.dto'
@@ -5,24 +6,30 @@ import { AddressDto } from '../../common/dtos/address.dto'
 export class CreateUserDto {
   @IsNotEmpty()
   @IsCPF()
+  @ApiProperty({ example: '123.456.789-01' })
   cpf: string
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'Fulano da Silva' })
   name: string
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({ example: 'fulano@company.com' })
   email: string
 
   @IsNotEmpty()
   @IsPhoneNumber('BR')
+  @ApiProperty({ example: '(53) 987412155' })
   phone: string
 
   @IsNotEmpty()
+  @ApiProperty()
   address: AddressDto
 
   @IsNotEmpty()
   @IsPositive()
+  @ApiProperty({ example: 1 })
   idCompany: number
 }
