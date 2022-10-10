502555f0e955416cfbbe767c2172525af4a7ce2e
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 13:39:17 -0300

    feat: add swagger properties decorators into AddressDto

--- src/common/dtos/address.dto.ts
+++ src/common/dtos/address.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsCEP } from 'brazilian-class-validator'
 import { IsNotEmpty, IsNumber } from 'class-validator'
 
 export class AddressDto {
   @IsNotEmpty()
   @IsCEP()
+  @ApiProperty({ example: '99999-999' })
   cep: string
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ example: 26 })
   num: number
 }
