229005e31bbc36782f7df4480db8743edc06d03b
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 11:28:15 -0300

    feat: import DataSource in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppDataSource } from './data-source'
 
 @Module({
-  imports: []
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.example' }),
+    TypeOrmModule.forRoot(AppDataSource.options)
+  ]
 })
 export class AppModule {}
