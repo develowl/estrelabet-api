38780370b56380f5a7436f091bbb8620078c6abe
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:50:39 -0300

    feat: add user entity missing fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,35 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Company } from '../../companies/entities/company.entity'
+import { sanitizeCPF } from '../../helpers'
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number
+
+  @Column({
+    unique: true,
+    length: 11,
+    transformer: {
+      to: (value: string) => value.replace(/\D/g, ''),
+      from: (value: string) => sanitizeCPF(value)
+    }
+  })
+  cpf: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  phone: string
+
+  @Column()
+  address: string
+
+  @ManyToOne(() => Company, (company) => company.id, { eager: true })
+  @JoinColumn({ name: 'id_company' })
+  company: Company
 }
