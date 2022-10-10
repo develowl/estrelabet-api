9bfa53e36c9a2931b5760d2ee9d884143d02d308
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 08:19:26 -0300

    feat: add get company function

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Inject } from '@nestjs/common'
+import { Controller, Get, Inject, Param } from '@nestjs/common'
 import { CompaniesService } from './companies.service'
+import { Company } from './entities/company.entity'
 
 @Controller('companies')
 export class CompaniesController {
   constructor(@Inject(CompaniesService) private readonly service: CompaniesService) {}
+
+  @Get(':id')
+  async get(@Param(':id') id: number): Promise<Company> {
+    return await this.service.get(id)
+  }
 }
