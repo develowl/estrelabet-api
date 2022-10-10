1cd5ff44aad5120afdde5e0cf65404673c250a23
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:54:00 -0300

    feat: add create user function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,18 @@
-import { NotFoundException } from '@nestjs/common'
-import { Injectable } from '@nestjs/common/decorators'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common/decorators'
 import { InjectRepository } from '@nestjs/typeorm'
+import { CompaniesService } from 'src/companies/companies.service'
 import { Repository } from 'typeorm'
+import { fetchAddress } from '../helpers'
+import { CreateUserDto } from './dto/create-user.dto'
 import { User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private readonly repo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+    @Inject(CompaniesService) private readonly companiesService: CompaniesService
+  ) {}
 
   async get(id: number): Promise<User> {
     try {
@@ -18,5 +24,18 @@ export class UsersService {
 
   async find(): Promise<User[]> {
     return await this.repo.find()
+  }
+
+  async create({ address: { cep, num }, idCompany, ...dto }: CreateUserDto): Promise<User> {
+    try {
+      const foundCompany = await this.companiesService.get(idCompany)
+      const address = await fetchAddress({ cep, num })
+      return await this.repo.save(this.repo.create({ ...dto, address, company: foundCompany }))
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error
+      }
+      throw new BadRequestException('Unable to create')
+    }
   }
 }
