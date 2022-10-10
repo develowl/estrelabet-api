01f54a1b98aaf5f347002b89f691c56001156bd1
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 13:39:29 -0300

    feat: add swagger properties decorators into CreateCompanyDto

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsCNPJ } from 'brazilian-class-validator'
 import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator'
 import { AddressDto } from '../../common/dtos/address.dto'
@@ -5,20 +6,25 @@ import { AddressDto } from '../../common/dtos/address.dto'
 export class CreateCompanyDto {
   @IsNotEmpty()
   @IsCNPJ()
+  @ApiProperty({ example: '11.111.111/1111-11' })
   cnpj: string
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({ example: 'Company S.A.' })
   name: string
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({ example: 'company@company.com' })
   email: string
 
   @IsNotEmpty()
   @IsPhoneNumber('BR')
+  @ApiProperty({ example: '(53) 987412155' })
   phone: string
 
   @IsNotEmpty()
+  @ApiProperty()
   address: AddressDto
 }
