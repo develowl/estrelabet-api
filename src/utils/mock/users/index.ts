d5f1cf68f69efc9ec49b217c1c7917acb947c1f6
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 09:52:45 -0300

    feat: add mock user helper

--- src/utils/mock/users/index.ts
+++ src/utils/mock/users/index.ts
@@ -1,5 +1,26 @@
+import { CreateUserDto } from '../../../users/dto/create-user.dto'
 import { User } from '../../../users/entities/user.entity'
+import { mockCompany } from '../company'
+
+const baseUserDto = {
+  cpf: '025474198475',
+  name: 'User',
+  email: 'user@company.com',
+  phone: '5198478874'
+}
+
+export const mockCreateUserDto: CreateUserDto = {
+  ...baseUserDto,
+  address: {
+    cep: '11111-111',
+    num: 26
+  },
+  idCompany: 1
+}
 
 export const mockUser: User = {
-  id: 1
+  id: 1,
+  ...baseUserDto,
+  address: 'Rua abc, 123, Jardim Planalto, Esteio - RS, 11111-111',
+  company: mockCompany
 }
