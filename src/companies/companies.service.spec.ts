a5c839729394ded129af751c675db1cfc9811f9a
Yan Gonçalves <fgyan94@gmail.com>
2022-10-09 22:21:45 -0300

    test: create company

--- src/companies/companies.service.spec.ts
+++ src/companies/companies.service.spec.ts
@@ -1,29 +1,18 @@
-import { NotFoundException } from '@nestjs/common'
+import { BadRequestException, NotFoundException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
+import mockFetch from 'jest-fetch-mock'
+import { mockCompany, mockCompanyDto } from '../utils/mock/company'
+import { mockConfigService } from '../utils/mock/configService'
+import { mockRepository } from '../utils/mock/repository'
 import { CompaniesService } from './companies.service'
 import { Company } from './entities/company.entity'
 
+mockFetch.enableMocks()
+
 describe('CompaniesService', () => {
   let companiesService: CompaniesService
-
-  const mockRepository = {
-    find: jest.fn(),
-    findOneByOrFail: jest.fn(),
-    create: jest.fn(),
-    save: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn()
-  }
-
-  const mockCompany = {
-    id: 1,
-    cnpj: '22.980.324/0001-43',
-    name: 'Company',
-    email: 'company@company.com.br',
-    phone: '+551194712544',
-    address: 'rua abc, 123, jardim fgh, São Paulo - SP'
-  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -32,6 +21,10 @@ describe('CompaniesService', () => {
         {
           provide: getRepositoryToken(Company),
           useValue: mockRepository
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService
         }
       ]
     }).compile()
@@ -39,15 +32,9 @@ describe('CompaniesService', () => {
     companiesService = await module.get(CompaniesService)
   })
 
-  afterEach(() => jest.clearAllMocks())
-
-  describe('find', () => {
-    it('should return an array of companies', async () => {
-      jest.spyOn(mockRepository, 'find').mockReturnValueOnce([mockCompany])
-
-      expect(await companiesService.find()).toHaveLength(1)
-      expect(mockRepository.find).toHaveBeenCalledTimes(1)
-    })
+  afterEach(() => {
+    jest.clearAllMocks()
+    fetchMock.mockClear()
   })
 
   describe('findOneByOrFail', () => {
@@ -58,12 +45,41 @@ describe('CompaniesService', () => {
       expect(mockRepository.findOneByOrFail).toHaveBeenCalledTimes(1)
     })
 
-    it('should throw an error when not found a valid company', async () => {
+    it('should throws an exception when not found a valid company', async () => {
       jest
         .spyOn(mockRepository, 'findOneByOrFail')
         .mockReturnValueOnce(new Promise((_, reject) => reject(new NotFoundException())))
 
       await expect(companiesService.get(mockCompany.id)).rejects.toThrow(NotFoundException)
+    })
+  })
+
+  describe('find', () => {
+    it('should return an array of companies', async () => {
+      jest.spyOn(mockRepository, 'find').mockReturnValueOnce([mockCompany])
+
+      expect(await companiesService.find()).toHaveLength(1)
+      expect(mockRepository.find).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('create/save', () => {
+    it('should return a new company', async () => {
+      mockFetch.mockResponseOnce(JSON.stringify(mockCompanyDto.address))
+      jest.spyOn(mockRepository, 'create').mockReturnValueOnce(mockCompany)
+      jest.spyOn(mockRepository, 'save').mockReturnValueOnce(mockCompany)
+
+      expect(await companiesService.create(mockCompanyDto)).toStrictEqual(mockCompany)
+      expect(mockRepository.create).toHaveBeenCalledTimes(1)
+      expect(mockRepository.save).toHaveBeenCalledTimes(1)
+    })
+
+    it('should throws an exception when creation goes wrong', async () => {
+      jest
+        .spyOn(mockRepository, 'save')
+        .mockReturnValueOnce(new Promise((_, reject) => reject(new BadRequestException())))
+
+      await expect(companiesService.create(mockCompanyDto)).rejects.toThrow(BadRequestException)
     })
   })
 })
