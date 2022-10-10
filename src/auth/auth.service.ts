95048d2c9d01e8ddc5270f28a4a1982a1545a284
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 20:59:31 -0300

    feat: add mock admin user in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common/decorators'
+import { Inject, Injectable } from '@nestjs/common/decorators'
+import { ConfigService } from '@nestjs/config'
+import { hashSync } from 'bcrypt'
+import { MockAdmin } from 'src/utils/mock/auth'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private mockAdmin: MockAdmin
+
+  constructor(@Inject(ConfigService) private readonly configService: ConfigService) {
+    this.mockAdmin = {
+      identifier: this.configService.get<string>('ADMIN_IDENTIFIER'),
+      password: hashSync(this.configService.get<string>('ADMIN_PASSWORD'), 10),
+      refreshToken: null
+    }
+  }
+}
