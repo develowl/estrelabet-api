f134ecc059383fc719332f1c502c412eed88c5c3
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 10:40:09 -0300

    feat: add UsersController get function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Inject } from '@nestjs/common'
+import { Controller, Get, Inject, Param } from '@nestjs/common'
+import { User } from './entities/user.entity'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
   constructor(@Inject(UsersService) private readonly service: UsersService) {}
+
+  @Get(':id')
+  async get(@Param('id') id: number): Promise<User> {
+    return await this.service.get(id)
+  }
 }
