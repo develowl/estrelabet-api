18fde827bc4ccf6c7a5fcfed6e4d41cdc34d3b33
Yan Gonçalves <fgyan94@gmail.com>
2022-10-11 01:35:52 -0300

    feat: add AuthController refreshTokens function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common'
 import { Jwt } from 'src/types/jwt'
 import { AuthService } from './auth.service'
+import { RefreshTokensDto } from './dto/refresh-tokens.dto'
 import { SigninDto } from './dto/signin.dto'
 
 @Controller('auth')
@@ -17,5 +18,10 @@ export class AuthController {
     @Body() { identifier }: Pick<SigninDto, 'identifier'>
   ): Promise<{ message: string }> {
     return await this.service.signout(identifier)
+  }
+
+  @Post('refresh-tokens')
+  async refreshTokens(@Body() { identifier, refreshToken }: RefreshTokensDto): Promise<Jwt> {
+    return await this.service.refreshTokens(identifier, refreshToken)
   }
 }
