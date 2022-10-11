7504b91d370c122e4123bf3c6540528c6c9c6a59
Yan Gonçalves <fgyan94@gmail.com>
2022-10-11 01:40:47 -0300

    feat: add properly private and public routes in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,24 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common'
-import { Jwt } from 'src/types/jwt'
+import { Body, Controller, Inject, Post, SetMetadata, UseGuards } from '@nestjs/common'
+import { Public } from '../decorators/public.route.decorator'
+import { Jwt } from '../types/jwt'
 import { AuthService } from './auth.service'
 import { RefreshTokensDto } from './dto/refresh-tokens.dto'
 import { SigninDto } from './dto/signin.dto'
+import { JwtAuthGuard } from './guards/jwt-auth.guard'
+import { JwtRefreshAuthGuard } from './guards/jwt-refresh-auth.guard'
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(AuthService) private readonly service: AuthService) {}
 
   @Post('signin')
+  @Public()
   async signin(@Body() signinDto: SigninDto): Promise<Jwt> {
     return await this.service.signin(signinDto)
   }
 
   @Post('signout')
+  @UseGuards(JwtAuthGuard)
   async signout(
     @Body() { identifier }: Pick<SigninDto, 'identifier'>
   ): Promise<{ message: string }> {
@@ -21,6 +26,8 @@ export class AuthController {
   }
 
   @Post('refresh-tokens')
+  @UseGuards(JwtRefreshAuthGuard)
+  @SetMetadata('jwt-refresh', true)
   async refreshTokens(@Body() { identifier, refreshToken }: RefreshTokensDto): Promise<Jwt> {
     return await this.service.refreshTokens(identifier, refreshToken)
   }
