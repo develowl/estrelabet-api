1aed237898487502dff621297b909e8271cc707d
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 22:03:00 -0300

    refactor: get function from mockConfigService

--- src/utils/mock/configService/index.ts
+++ src/utils/mock/configService/index.ts
@@ -1,6 +1,14 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 export const mockConfigService = {
   get(key: string) {
-    return 'https://viacep.com.br/ws'
+    switch (key) {
+      case 'JWT_SECRET':
+        return 'abc'
+      case 'JWT_REFRESH_SECRET':
+        return '123'
+      case 'ADMIN_IDENTIFIER':
+        return 'admin'
+      case 'ADMIN_PASSWORD':
+        return '1@EstrelaBet'
+    }
   }
 }
