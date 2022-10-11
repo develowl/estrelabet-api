7dac7d36c31f277da99da410c48d7d7e2e5ffd24
Yan Gonçalves <fgyan94@gmail.com>
2022-10-11 01:00:29 -0300

    test: ensure AuthController signin successfully

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,6 +1,8 @@
 import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
+import { MockAdmin } from '../types/auth'
+import { Jwt } from '../types/jwt'
 import { mockConfigService } from '../utils/mock/configService'
 import { mockJwtService } from '../utils/mock/jwt'
 import { AuthController } from './auth.controller'
@@ -9,6 +11,8 @@ import { AuthService } from './auth.service'
 describe('AuthController', () => {
   let authController: AuthController
   let authService: AuthService
+  const mockAdmin: MockAdmin = { identifier: 'admin', password: 'estrelabet', refreshToken: '123' }
+  const mockTokens: Jwt = { access_token: 'abc', refresh_token: '123' }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -36,5 +40,15 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(authController).toBeDefined()
+  })
+
+  describe('signin', () => {
+    it('should signin successfully', async () => {
+      const spySignin = jest.spyOn(authController, 'signin')
+      jest.spyOn(authService, 'signin').mockResolvedValueOnce(mockTokens)
+
+      expect(await authController.signin(mockAdmin)).toStrictEqual(mockTokens)
+      expect(spySignin).toHaveBeenCalledWith(mockAdmin)
+    })
   })
 })
