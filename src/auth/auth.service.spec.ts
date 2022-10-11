2ff388657dd812eae268907fda57af03cd0201f5
Yan Gonçalves <fgyan94@gmail.com>
2022-10-10 22:07:02 -0300

    test: ensure AuthService signin successfully

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,10 +1,22 @@
 import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
+import * as bcrypt from 'bcrypt'
+import { MockAdmin } from 'src/types/auth'
+import { Jwt } from 'src/types/jwt'
 import { mockConfigService } from '../utils/mock/configService'
+import { mockJwtService } from '../utils/mock/jwt'
 import { AuthService } from './auth.service'
+
+jest.mock('bcrypt', () => ({
+  ...jest.requireActual('bcrypt'),
+  compare: jest.fn()
+}))
 
 describe('AuthService', () => {
   let authService: AuthService
+  const mockAdmin: MockAdmin = { identifier: 'admin', password: '1@EstrelaBet' }
+  const mockTokens: Jwt = { access_token: 'abc', refresh_token: '123' }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,6 +25,10 @@ describe('AuthService', () => {
         {
           provide: ConfigService,
           useValue: mockConfigService
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService
         }
       ]
     }).compile()
@@ -20,7 +36,22 @@ describe('AuthService', () => {
     authService = module.get<AuthService>(AuthService)
   })
 
+  afterEach(() => jest.clearAllMocks())
+
   it('should be defined', () => {
     expect(authService).toBeDefined()
+  })
+
+  describe('signin', () => {
+    it('should signin successfully', async () => {
+      const spyGetAdmin = jest.spyOn(authService, 'getAdmin')
+      const spyGetTokens = jest.spyOn(AuthService.prototype as any, 'getTokens')
+
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(async () => Promise.resolve(true))
+
+      expect(await authService.signin(mockAdmin)).toStrictEqual(mockTokens)
+      expect(spyGetAdmin).toHaveBeenCalledWith(mockAdmin.identifier)
+      expect(spyGetTokens).toHaveBeenCalledWith({ identifier: mockAdmin.identifier })
+    })
   })
 })
