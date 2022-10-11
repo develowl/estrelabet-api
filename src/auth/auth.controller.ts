2bd05c139482575cdd20bbd9319a6dc46d5f4837
Yan Gonçalves <fgyan94@gmail.com>
2022-10-11 00:59:46 -0300

    feat: add AuthController signin function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Inject } from '@nestjs/common'
+import { Body, Controller, Inject, Post } from '@nestjs/common'
+import { Jwt } from 'src/types/jwt'
 import { AuthService } from './auth.service'
+import { SigninDto } from './dto/signin.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(AuthService) private readonly service: AuthService) {}
+
+  @Post()
+  async signin(@Body() signinDto: SigninDto): Promise<Jwt> {
+    return await this.service.signin(signinDto)
+  }
 }
