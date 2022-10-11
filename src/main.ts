5dd34e653d99f919753bfed901864b144980b0d9
Yan Gonçalves <fgyan94@gmail.com>
2022-10-11 01:32:31 -0300

    refactor: organize responsabilities

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,18 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
   const configService = app.get(ConfigService)
 
   const config = new DocumentBuilder()
     .setTitle('EstrelaBet API')
     .setDescription('The challenge EstrelaBet API')
     .setVersion('1.0')
+    .addBearerAuth()
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
+
+  app.useGlobalPipes(new ValidationPipe())
 
   const port = Number(configService.get<string>('PORT')) || 3000
   await app.listen(port)
